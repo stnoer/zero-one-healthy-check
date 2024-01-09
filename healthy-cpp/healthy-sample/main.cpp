9619073fea44e544ad62cba1f5d76e3dada130d3
awei <2069682479@qq.com>
2024-01-09 21:40:54 +0800

    新增Excel报表使用案例

--- healthy-cpp/healthy-sample/main.cpp
+++ healthy-cpp/healthy-sample/main.cpp
@@ -33,6 +33,7 @@
 #include "ServerInfo.h"
 #include "NacosClient.h"
 #include "FastDfsClient.h"
+#include "ExcelComponent.h"
 
 // 是否是发布Swagger文档包
 #ifndef _RELEASE_DOC_
@@ -171,6 +172,61 @@ void testDfs(string fileName)
 // 	}
 }
 
+void testExcel()
+{
+	// 创建测试数据
+	std::vector<std::vector<std::string>> data;
+	stringstream ss;
+	for (int i = 1; i <= 10; i++)
+	{
+		std::vector<std::string> row;
+		for (int j = 1; j <= 5; j++)
+		{
+			ss.clear();
+			ss << "pos(" << i << "," << j << ")";
+			row.push_back(ss.str());
+			ss.str("");
+		}
+		data.push_back(row);
+	}
+
+	// 定义保存数据位置和页签名称
+	// 注意：文件件名称和文件路径不能出现中文
+	std::string fileName = "./public/excel/1.xlsx";
+	// 注意：因为xlnt不能存储非utf8编码的字符，所以中文字需要从配置文件中获取
+	std::string sheetName = ZH_WORDS_GETTER("excel.sheet.s1");
+
+	// 保存到文件
+	ExcelComponent excel;
+	excel.writeVectorToFile(fileName, sheetName, data);
+
+	// 从文件中读取
+	auto readData = excel.readIntoVector(fileName, sheetName);
+	for (auto row : readData)
+	{
+		for (auto cellVal : row)
+		{
+			cout << cellVal << ",";
+		}
+		cout << endl;
+	}
+
+	// 测试创建第二个页签
+	sheetName = ZH_WORDS_GETTER("excel.sheet.s2");
+	excel.writeVectorToFile(fileName, sheetName, data);
+
+	// 测试覆盖第一个页签
+	sheetName = ZH_WORDS_GETTER("excel.sheet.s1");
+	data.insert(data.begin(), {
+		ZH_WORDS_GETTER("excel.header.h1") ,
+		ZH_WORDS_GETTER("excel.header.h2") ,
+		ZH_WORDS_GETTER("excel.header.h3") ,
+		ZH_WORDS_GETTER("excel.header.h4") ,
+		ZH_WORDS_GETTER("excel.header.h5") ,
+		});
+	excel.writeVectorToFile(fileName, sheetName, data);
+}
+
 int main(int argc, char* argv[]) {
 #ifdef HTTP_SERVER_DEMO
 	// 测试生成 JWT Token
@@ -179,7 +235,8 @@ int main(int argc, char* argv[]) {
 
 	// 服务器参数初始化
 	bool isSetDb = getStartArg(argc, argv);
-	testDfs("E:\\Images\\20141011112404344.jpg.source.jpg");
+	//testDfs("E:\\Images\\20141011112404344.jpg.source.jpg");
+	testExcel();
 #ifdef LINUX
 	// 创建Nacos客户端对象
 	NacosClient nacosClient(
