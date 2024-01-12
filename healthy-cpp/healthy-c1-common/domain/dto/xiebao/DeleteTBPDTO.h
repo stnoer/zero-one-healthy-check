b11cf62cf6ac291ea134b45d2ffde5b3e1f168a4
zebraG <1033416242@qq.com>
2024-01-13 02:30:03 +0800

    新增删除基础项目、修改基础项目接口定义更改模块名称

---
+++ healthy-cpp/healthy-c1-common/domain/dto/xiebao/DeleteTBPDTO.h
@@ -0,0 +1,30 @@
+#pragma once
+#ifndef _DeleteTBPDTO_H_
+#define _DeleteTBPDTO_H_
+#include"../../GlobalInclude.h"
+#include "tree/TreeNode.h"
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+/**
+ * 定义一个示例菜单显示JsonVO对象，用于响应给前端
+ */
+class DeleteTBPDTO : public oatpp::DTO,public TreeNode
+{
+    DTO_INIT(DeleteTBPDTO, DTO);
+
+    // 项目代码
+    API_DTO_FIELD_DEFAULT(String, code, ZH_WORDS_GETTER("code"));
+    // 是否已删除
+    API_DTO_FIELD_DEFAULT(int, del_flag, ZH_WORDS_GETTER("del_flag"));
+public:
+    void addChild(shared_ptr<TreeNode> child) override
+    {
+        children->push_back(Wrapper(dynamic_pointer_cast<DeleteTBPDTO>(child), Wrapper::Class::getType()));
+    }
+};
+
+
+#include OATPP_CODEGEN_END(DTO)
+
+#endif // !_DeleteTBPDTO_H_
