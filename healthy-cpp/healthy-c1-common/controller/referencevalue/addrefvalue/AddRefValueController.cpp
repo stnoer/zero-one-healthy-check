9e2b08eb1024ab1e4e90a48532b6f0a1757c6f28
2521054164 <2521054164@qq.com>
2024-01-13 23:22:22 +0800

    添加新增指定项目参考值模块的Controller层文件

---
+++ healthy-cpp/healthy-c1-common/controller/referencevalue/addrefvalue/AddRefValueController.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "AddRefValueController.h"
+
+Uint64JsonVO::Wrapper AddRefValueController::execAddRefValue(const AddRefValueDTO::Wrapper& dto)
+{
+	return Uint64JsonVO::Wrapper();
+}
