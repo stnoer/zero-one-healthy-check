1391b99b31f7cf0a5cd472b4b779f524fcf05d89
2521054164 <2521054164@qq.com>
2024-01-14 14:30:29 +0800

    完善DTO层

--- healthy-cpp/healthy-c1-common/domain/dto/referencevalue/RefValueDTO.h
+++ healthy-cpp/healthy-c1-common/domain/dto/referencevalue/RefValueDTO.h
@@ -29,7 +29,7 @@ class RefValueDTO : public oatpp::DTO
 {
 	DTO_INIT(RefValueDTO, DTO);
 	// base_project_id 项目id
-	API_DTO_FIELD_DEFAULT(String, type, ZH_WORDS_GETTER("common.reference_value.field.base_project_id"));
+	API_DTO_FIELD_DEFAULT(String, base_project_id, ZH_WORDS_GETTER("common.reference_value.field.base_project_id"));
 	// allow_sex 适合性别
 	API_DTO_FIELD_DEFAULT(String, allow_sex, ZH_WORDS_GETTER("common.reference_value.field.allow_sex"));
 	// min_age 年龄最低值
