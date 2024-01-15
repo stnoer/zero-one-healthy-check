c4010691defb3d6bbdc88324f1d723938263bee9
stoner <2310364446@qq.com>
2024-01-15 15:16:40 +0800

    新增分页查询危急值接口

---
+++ healthy-cpp/healthy-c1-common/controller/database/CritValueController.cpp
@@ -0,0 +1,10 @@
+#include "CritValueController.h"
+#include "../../domain/vo/database/CritValueVO.h"
+#include "../../domain/query/database/CritValuePageQuery.h"
+
+
+
+CritValueJsonPageVO::Wrapper CritValueController::execQueryPageCritValue(const CritValuePageQuery::Wrapper& quary)
+{
+	return {};
+}
