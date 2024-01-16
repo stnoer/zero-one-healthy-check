6f634f6102d9626128bee385d5f4bdef4aa28af8
zebraG <1033416242@qq.com>
2024-01-16 18:36:15 +0800

    完成 体检套餐-修改套餐、删除套餐、新增套餐 接口定义部分

--- healthy-cpp/healthy-c1-common/controller/combo/ComboController.cpp
+++ healthy-cpp/healthy-c1-common/controller/combo/ComboController.cpp
@@ -16,3 +16,31 @@ Uint64JsonVO::Wrapper ComboController::execAddPack(const AddPackDTO::Wrapper& dt
 	//响应结果
 	return jvo;
 }
+
+Uint64JsonVO::Wrapper ComboController::execDelPack(const DelPackDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 参数校验
+	// 非空校验
+
+	// 定义一个Service
+	// 执行数据新增
+
+	//响应结果
+	return jvo;
+}
+
+Uint64JsonVO::Wrapper ComboController::execUpdatePack(const UpdatePackDTO::Wrapper& dto)
+{
+	// 定义返回数据对象
+	auto jvo = Uint64JsonVO::createShared();
+	// 参数校验
+	// 非空校验
+
+	// 定义一个Service
+	// 执行数据新增
+
+	//响应结果
+	return jvo;
+}
--- healthy-cpp/healthy-c1-common/controller/combo/ComboController.h
+++ healthy-cpp/healthy-c1-common/controller/combo/ComboController.h
@@ -5,6 +5,8 @@
 #include "oatpp-swagger/Types.hpp"
 #include "domain/vo/BaseJsonVO.h"
 #include "domain/vo/combo/AddPackVO.h"
+#include "domain/vo/combo/DelPackVO.h"
+#include "domain/vo/combo/UpdatePackVO.h"
 using namespace oatpp;
 
 #include OATPP_CODEGEN_BEGIN(ApiController) //<- Begin Codegen
@@ -29,9 +31,43 @@ public:// 定义接口
 		// 呼叫执行函数响应结果
 		API_HANDLER_RESP_VO(execAddPack(dto));
 	}
+
+	// 3.1 定义新增接口描述
+	ENDPOINT_INFO(delPack) {
+		// 定义接口标题
+		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("combo.post.delPack"));
+		// 定义默认授权参数（可选定义，如果定义了，下面ENDPOINT里面需要加入API_HANDLER_AUTH_PARAME）
+		API_DEF_ADD_AUTH();
+		// 定义响应参数格式
+		API_DEF_ADD_RSP_JSON_WRAPPER(Uint64JsonVO);
+	}
+	// 3.2 定义新增接口处理
+	ENDPOINT(API_M_POST, "/combo/del-pack", delPack, BODY_DTO(DelPackDTO::Wrapper, dto), API_HANDLER_AUTH_PARAME) {
+		// 呼叫执行函数响应结果
+		API_HANDLER_RESP_VO(execDelPack(dto));
+	}
+
+	// 3.1 定义新增接口描述
+	ENDPOINT_INFO(updatePack) {
+		// 定义接口标题
+		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("combo.post.updatePack"));
+		// 定义默认授权参数（可选定义，如果定义了，下面ENDPOINT里面需要加入API_HANDLER_AUTH_PARAME）
+		API_DEF_ADD_AUTH();
+		// 定义响应参数格式
+		API_DEF_ADD_RSP_JSON_WRAPPER(Uint64JsonVO);
+	}
+	// 3.2 定义新增接口处理
+	ENDPOINT(API_M_POST, "/combo/update-pack", updatePack, BODY_DTO(UpdatePackDTO::Wrapper, dto), API_HANDLER_AUTH_PARAME) {
+		// 呼叫执行函数响应结果
+		API_HANDLER_RESP_VO(execUpdatePack(dto));
+	}
 private:// 定义接口执行函数
 	//新增数据
 	Uint64JsonVO::Wrapper execAddPack(const AddPackDTO::Wrapper& dto);
+	//删除数据
+	Uint64JsonVO::Wrapper execDelPack(const DelPackDTO::Wrapper& dto);
+	//修改数据
+	Uint64JsonVO::Wrapper execUpdatePack(const UpdatePackDTO::Wrapper& dto);
 };
 
 #include OATPP_CODEGEN_END(ApiController) //<- End Codegen
