7883debaf6f539d5d0b7288ee8b44f24118f91fe
zebraG <1033416242@qq.com>
2024-01-28 21:46:59 +0800

    修复报错，目前能够正常运行

--- healthy-cpp/healthy-c1-common/controller/rules/RulesListController.h
+++ healthy-cpp/healthy-c1-common/controller/rules/RulesListController.h
@@ -36,15 +36,22 @@ class RulesListController : public oatpp::web::server::api::ApiController
 public:
 	// 定义查询所有规则信息接口端点描述
 	ENDPOINT_INFO(queryAllRules) {
-		// 定义接口通用信息
-		API_DEF_ADD_COMMON_AUTH(ZH_WORDS_GETTER("rules.query-all.summary"), RulesListJsonVO::Wrapper);
+		// 定义接口标题
+		API_DEF_ADD_TITLE(ZH_WORDS_GETTER("rules.query-all.summary"));
+		// 定义默认授权参数（可选定义，如果定义了，下面ENDPOINT里面需要加入API_HANDLER_AUTH_PARAME）
+		API_DEF_ADD_AUTH();
+		// 定义响应参数格式
+		API_DEF_ADD_RSP_JSON_WRAPPER(RulesListJsonVO);
 		// 定义分页查询参数描述
 		API_DEF_ADD_PAGE_PARAMS();
-		//// 定义其他查询参数描述
-		//API_DEF_ADD_QUERY_PARAMS(String, "shortname", ZH_WORDS_GETTER("rules.field.shortname"), "li ming", true);
 	}
-	// 定义查询所有用户信息接口端点处理
-	API_HANDLER_ENDPOINT_QUERY_AUTH(API_M_GET, "/rules/query-all", queryAllRules, RulesListPageQuery, executeQueryAll(query));
+	// 3.2 定义查询接口处理
+	ENDPOINT(API_M_GET, "/rules/query-all", queryAllRules, QUERIES(QueryParams, queryParams), API_HANDLER_AUTH_PARAME) {
+		// 解析查询参数为Query领域模型
+		API_HANDLER_QUERY_PARAM(userQuery, RulesListQuery, queryParams);
+		// 呼叫执行函数响应结果
+		API_HANDLER_RESP_VO(executeQueryAll(userQuery));
+	}
 
 	// 定义查询所有规则信息接口端点描述
 	ENDPOINT_INFO(queryRules) {
